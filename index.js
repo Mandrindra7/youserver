2c3e6ff02a859829982ad97a12219ae58f80432a
mandrindra7 <herymandrindra@gmail.com>
2021-05-20 09:35:12 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send("Hello word")
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=>console.log(`Server Running on Port ${PORT}...`))
